4216947f8be64ce3b81cf1000bf2ff391d5d6c3c
praniil <pparajuli15@gmail.com>
2025-03-17 19:42:41 +0545

    implemented queue and stack, tried list comprehension and implemented del keyword to delete a list item and enumerate() method to iterate through the list

--- src/data_structures/lists.py
+++ src/data_structures/lists.py
@@ -1,4 +1,5 @@
 # import numpy as np
+from collections import deque
 
 list1 = {1, 2, 3, 'a', 'b', 'c', 1}
 list1.remove(2)
@@ -145,3 +146,61 @@ print(non_sorted_string)
 fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'banana', 'apple', 'banana']
 search_index = fruits.index('banana', 4)    #find next banana starting at position 4
 print(search_index)
+
+#list as a stack
+stack = [1, 2, 3, 4]
+stack.append(5)
+print(stack)
+stack.pop()
+print(stack)
+
+#list as a queue
+queue = deque(["pranil", "nil", "is"])
+queue.append("goat")
+print(queue)
+queue.popleft()
+print(queue)
+
+#list comprehension
+old_list = [1, 2, 3, 4, 5]
+new_list = []
+for i23 in old_list:
+    new_list.append(i23**2)
+
+print("new_list: ", new_list)
+print(i23)  #i23 is still available here after the for loop ends wtffff
+
+#side effect like i23 is not seen
+new_list2 = [x23**2 for x23 in old_list]
+print("new_list2: ", new_list2) 
+
+less_than_list = []
+less_than_list.append(set((x, y) for x in [1, 2, 3] for y in [5, 6, 7] if x < y))
+print(less_than_list)
+
+num_and_square = []
+print(list((x, x**2) for x in range(5)))
+num_and_square.append(list((x, x**2) for x in range(5)))
+print(num_and_square)
+
+
+#del a[0]
+del_list = [1, 2, 3, 4, 5]
+del del_list[0]
+print(del_list)
+
+#testing something
+testing_list = [(a, b) for a in [1, 2, 3] for b in [5, 6, 7] if a < b]
+print(testing_list)
+
+testing_list2 = []
+testing_list2.append([(a, b) for a in [1, 2, 3] for b in [5, 6, 7] if a < b])
+print("testing list", testing_list2)
+
+#iterate through list
+iter_list = [1, 2, 3, 4, "pranil"]
+for i in iter_list:
+    print(i) #gives the item not the index
+
+for (i, v) in enumerate(iter_list):
+    print(f"iter_list[{i}] = {v}")
