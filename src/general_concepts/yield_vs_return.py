9787d38dd424f487db31b877f98c75bf1068d8ec
praniil <pparajuli15@gmail.com>
2026-07-19 21:23:23 +0545

    learn yield vs return

--- src/general_concepts/yield_vs_return.py
+++ src/general_concepts/yield_vs_return.py
@@ -1,14 +1,23 @@
 def fun(number):
     for i in range(number):
-        yield i * i
+        yield i * i     #returns a generator
     
 def fun1(number):
     for i in range(number):
         return i * i
 
-list = list(fun(7))     #outputs list of squares of number upto 6
-print(list)
+gen = fun(3)
+print(next(gen))
+print(fun1(3))
+print(next(gen))
 
-list2 = fun1(7)
-print(list2)    #outputs 0
-    
+#normal list
+square_list = [x ** 2 for x in range(1, 5)]
+#generator
+square_list_gen = (x ** 2 for x in range(1, 5))
+
+print(square_list)
+print(next(square_list_gen))
+print(next(square_list_gen))
+print(next(square_list_gen))
+print(next(square_list_gen))
